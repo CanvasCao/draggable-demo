c385781e32d8c30c2330b8ad5b0b714b6d267f8b
Cao Yuhao <1174478646@qq.com>
2019-11-04 16:24:39 +0800

    [Yuhao] change state data to json file

--- src/pages/06TrelloDemo/index.js
+++ src/pages/06TrelloDemo/index.js
@@ -4,40 +4,10 @@ import HTML5Backend from 'react-dnd-html5-backend/lib/index'
 import {DndProvider} from 'react-dnd'
 import Storycard from './storycard'
 import _ from 'lodash'
+import storycards from '../../data/storycards2.json'
 
 class Index extends React.Component {
-    state = {
-        columns: [
-            {
-                id: 1,
-                name: 'Ready For Dev',
-                storyCards: [
-                    {id: 1, name: 'Story card 1'},
-                    {id: 2, name: 'Story card 2'},
-                ],
-            },
-            {
-                id: 2,
-                name: 'In Dev',
-                storyCards: [
-                    {id: 3, name: 'Story card 3'},
-                ],
-            },
-            {
-                id: 3,
-                name: 'Ready For QA',
-                storyCards: [],
-            },
-            {
-                id: 4,
-                name: 'In QA',
-                storyCards: [
-                    {id: 6, name: 'Story card 6'},
-                    {id: 7, name: 'Story card 7'},
-                ],
-            },
-        ],
-    }
+    state = storycards
 
     handleStoryCardChangeColumn = ({storyCardId, targetColumnId}) => {
         let columns = [...this.state.columns]
@@ -58,24 +28,26 @@ class Index extends React.Component {
     render() {
         return (
             <DndProvider backend={HTML5Backend}>
-                {this.state.columns.map((column) =>
-                    <Column
-                        key={column.id}
-                        columnName={column.name}
-                        columnId={column.id}
-                        columns={this.state.columns}
-                        storycards={column.storyCards}
-                    >
-                        {column.storyCards.map((storycard) =>
-                            <Storycard
-                                key={storycard.id}
-                                storyCardId={storycard.id}
-                                storyCardIdName={storycard.name}
-                                handleStoryCardChangeColumn={this.handleStoryCardChangeColumn}
-                            />
-                        )}
-                    </Column>
-                )}
+                <div id="columns-container">
+                    {this.state.columns.map((column) =>
+                        <Column
+                            key={column.id}
+                            columnName={column.name}
+                            columnId={column.id}
+                            columns={this.state.columns}
+                            storycards={column.storyCards}
+                        >
+                            {column.storyCards.map((storycard) =>
+                                <Storycard
+                                    key={storycard.id}
+                                    storyCardId={storycard.id}
+                                    storyCardIdName={storycard.name}
+                                    handleStoryCardChangeColumn={this.handleStoryCardChangeColumn}
+                                />
+                            )}
+                        </Column>
+                    )}
+                </div>
             </DndProvider>
         )
     }
